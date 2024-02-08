4d53dfcf0222b5d702409983dadcd0b285cf4129
Amar Jyothi <86199015+amarjyothi98@users.noreply.github.com>
2024-02-08 19:58:56 +0530

    updated index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Update imports
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render( // Use createRoot instead of ReactDOM.render
-  <Router>
-    <Routes>
-      <Route path="vercel.app/location/:id" element={<App />} /> {/* Wrap Route inside Routes */}
-    </Routes>
-  </Router>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
