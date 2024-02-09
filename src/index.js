8b43d62039e16585e3ce92822bfb4a0246119559
amarjyothi98 <amarjyothi526@gmail.com>
2024-02-09 10:40:11 +0530

    updated navigate

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 
 import App from './App';
+import First from './First';
 
 ReactDOM.createRoot(document.getElementById('root')).render( 
   <Router>
     <Routes>
+      <Route path='/' element={<First />}/>
       <Route path="/location/:id" element={<App />} /> 
     </Routes>
   </Router>,
